baecb87cf4aa1bf39c7beeecff15317234e145bc
Gustavo Schmeier <gustavo.bs10@hotmail.com>
2024-11-27 22:57:38 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada de dados")
+
+# Variáveis
+
+idade = 25
+nome = "Gustavo"
+
+# Print
+print(idade)
+
+# Soma
+
+nova_idade = idade + 5
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista.reverse()  # O método reverse altera a lista original
+print(lista)  # Agora a lista está invertida
